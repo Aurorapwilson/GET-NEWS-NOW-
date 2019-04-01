fb0e26c54f9c3d5c34f231faccab4b261007724e
Princess Wilson <aurorapwilson1@gmail.com>
2019-03-31 20:09:39 -0400

    localhost running

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const mongodb = require('mongodb');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
 //axios scraper
-const axios = require('axios');
+const axios = require("axios");
 // jquery for backend
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
 //require models
-const db = require('./models');
+const db = require("./models");
 //express server
 const app = express();
 //local server port
-let PORT =process.env.PORT || 8080;
+let PORT =process.env.PORT || 8000;
 
 
 
 
 //  //middleware
-app.use(logger);
+app.use(logger("combined"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -28,13 +28,13 @@ app.use(express.static("public"));
 
 
 //connect online mongolabdb
-mongoose.connect(" mongoose.connect('mongodb://princess:rent1234@ds125616.mlab.com:25616/heroku_sk1554w4", {useNewUrlParser: true});
+mongoose.connect("mongoose.connect('mongodb://princess:rent1234@ds125616.mlab.com:25616/heroku_sk1554w4", {useNewUrlParser: true});
 
 //Create GET route to scrape 
 app.get("/scrape", function(req,res){
 
     //GET request to URL through Axios 
-    axios.get('https://m.huffpost.com/us/section/world-news').then(function(response) {
+    axios.get("https://m.huffpost.com/us/section/world-news").then(function(response) {
     //load our URL request into server query selector
     let $ = cheerio.load(response.data);
 
@@ -56,10 +56,27 @@ app.get("/scrape", function(req,res){
             console.log(dbArticle);
         }).catch(function(err) {
         console.log(err);
+        });
     });
+
+    //lets client know when scrape is complete
+    res.send("Scrape Complete");
+        });
     });
-});
-});
+
+    //Route for getting all Articles from the db
+    app.get("/articles", function(req, res){
+        //we want to find all articles in our collection
+        db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        }).catch(function(error){
+        res.json(error);
+        });
+    });
+
+    
+    
 
 
 
